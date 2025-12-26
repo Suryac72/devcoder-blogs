09869bcdbb6dc760cc0dca58a9fcd9f2b53b63a8
Surya Prakash Chaudhary <suryac72@gmail.com>
2025-12-26 21:20:30 +0530

    Fix:config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',      
-  basePath: '/devocoder-knowledge-base', 
-  assetPrefix: '/devocoder-knowledge-base/',
+const config = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/devcoder-knowledge-base',
+  assetPrefix: '/devcoder-knowledge-base/',
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default withMDX(config);
